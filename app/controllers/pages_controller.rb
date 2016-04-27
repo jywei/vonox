89aa0686f5ee01b30ca4e7ba1e9711042885d8ed
Roy Wei <roywjy@gmail.com>
2016-04-27 12:49:57 +0800

    contact page is done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,9 @@ class PagesController < ApplicationController
   end
 
   def contact
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+    end
   end
 end
